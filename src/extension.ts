6c6799ce44c226c288b8069c426fbf0d14c85432
apommel <aurelien.pommel@gmail.com>
2020-01-12 21:49:46 +0900

    First functionalities implemented

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,17 +11,52 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "matlab-interactive-terminal" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const ext_dir = context.asAbsolutePath('');
+	const script_dir = path.join(ext_dir, "/src/python_scripts/");
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	const openMlTerminal = () => {
+		var script_path = path.join(script_dir, "ml_terminal.py");
+		const terminal = vscode.window.createTerminal({ name: 'Matlab REPL'});
+		terminal.sendText("python " + script_path);
+		terminal.show();
+	};
+	context.subscriptions.push(vscode.commands.registerCommand('extension.openMatlabTerminal', openMlTerminal));
 
-	context.subscriptions.push(disposable);
+	const runMlScript = () => {
+		if (vscode.window.activeTextEditor == undefined)
+		{
+			if (vscode.window.activeTerminal == undefined)
+			{
+				openMlTerminal();
+			}
+			else if (vscode.window.activeTerminal.name != "Matlab REPL")
+			{
+				openMlTerminal();
+			};
+		}
+		else
+		{
+			var current_file = path.basename(vscode.window.activeTextEditor.document.fileName);
+			var script_path = path.join(script_dir, "ml_script.py");
+			if (vscode.window.activeTerminal == undefined)
+			{
+				const terminal = vscode.window.createTerminal({ name: 'Matlab REPL'});
+				terminal.sendText("python " + script_path + ' ' + current_file);
+				terminal.show();
+			}
+			else if (vscode.window.activeTerminal.name == "Matlab REPL")
+			{
+				vscode.window.activeTerminal.sendText(path.parse(current_file).name)
+			}
+			else
+			{
+				const terminal = vscode.window.createTerminal({ name: 'Matlab REPL'});
+				terminal.sendText("python " + script_path + ' ' + current_file);
+				terminal.show();
+			};
+		}
+	};
+	context.subscriptions.push(vscode.commands.registerCommand('extension.runMatlabScript', runMlScript));
 }
 
 // this method is called when your extension is deactivated
---
+++ src/python_scripts/ml_script.py
@@ -0,0 +1,27 @@
+import sys
+try: # Check if the Matlab Engine is installed
+    import matlab.engine
+except:
+    print("Matlab Engine for Python cannot be detected. Please install it for the extension to work")
+    fail = True
+else:
+    fail = False
+    # Starting the Matlab engine
+    eng = matlab.engine.start_matlab()
+
+    # Run the initial script given in arg
+    eng.run(sys.argv[1], nargout=0)
+
+loop=True # Looping allows for an interactive terminal
+while loop and not fail:
+    command = input()
+    if command=="exit" or command=="exit()": # Keywords to leave the engine
+        loop=False
+    else:
+        try:
+            eng.eval(command, nargout=0) # Feed the instructions to Matlab eval
+        except: # The exceptions are actually handled by Matlab
+            pass
+
+if not fail:
+    eng.quit()
