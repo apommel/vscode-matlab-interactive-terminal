3fd68b92b1b57c699a1c104ba03ad904525d5d69
Nicholas Zhaosi <weihl18@lzu.edu.cn>
2020-10-18 23:34:23 +0800

    Implemented "Run Code Block" funcitonality.

---
+++ Test-Run-Code-Block.m
@@ -0,0 +1,11 @@
+%% The extension "weihongliang233.code-block" requiers that the first line in the editor should not be the splitor.
+1+1
+
+%**%
+disp("This is the 2th block")
+
+%**%
+disp("This is the 3rd block")
+
+%**%
+plot(sin([1:0.01:10]))
